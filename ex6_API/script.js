e757127c472975f87878bbc681f16630a516eb76
Weerapat013 <6340011013@psu.ac.th>
2022-09-15 23:19:35 +0700

    first commit

--- ex6_API/script.js
+++ ex6_API/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     var myHeaders = new Headers();
     myHeaders.append("apikey", "m3K70PQtu3ZsqRKKw6B7kTXQccV9PzNf");
 
@@ -16,10 +16,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
         let amountTHB = document.querySelector("#amount-THB").value;
         let amountEUR = 0;
-
         amountEUR = (amountTHB / rate).toFixed(2);
 
         document.querySelector("#amount-EUR").innerHTML = amountEUR;
     })
     .catch(error => console.log('error', error));
+});*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#conv").onsubmit = function () {
+        var myHeaders = new Headers();
+        myHeaders.append("apikey", "m3K70PQtu3ZsqRKKw6B7kTXQccV9PzNf");
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: myHeaders
+        };
+
+        fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=thb&base=eur", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const rate = result.rates.THB
+            document.querySelector("#exchangeRate").innerHTML = `1 EUR = ${rate} baht`;
+            let amountTHB = document.querySelector("#amount-THB").value;
+
+            function convert () {
+                return amountTHB / rate;
+            }
+            
+            //let amountEUR = 0;
+            //amountEUR = (amountTHB / rate).toFixed(2);
+            //document.querySelector("#amount-EUR").innerHTML = amountEUR;
+
+            document.querySelector("#amount-EUR").innerHTML = `${convert().toFixed(2)}`;
+        })
+        .catch(error => console.log('error', error));
+        return false;
+    };
 });
