480fc6a63679b06d7785df5f9b4c1e0e4b38d79f
Weerapat013 <6340011013@psu.ac.th>
2022-09-17 01:09:15 +0700

    first commit

--- Web_Project/script.js
+++ Web_Project/script.js
@@ -1,17 +1,53 @@
+// บูรพา - รับผิดชอบ
+
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#registerForm").onsubmit = function(){
+        let id = document.querySelector("#id").value;
+        let tel = document.querySelector("#tel").value;
         let username = document.querySelector("#username").value;
         let password = document.querySelector("#password").value;
         let confirmpassword = document.querySelector("#confirmpassword").value;
+        let checkID = false;
+        let checkTel = false;
         let checkUsername = false;
         let checkPassword = false;
         let checkConfirmPassword = false;
         let check = false;
+        let errMesID = "";
+        let errMesTel = "";
         let errMesUsername = "";
         let errMesPassword = "";
         let errMesConfirmPassword = "";
 
         //alert("Username: " + username + " Password: " + password);
+
+        if(id.length >= 13){
+            checkID = true;
+        }else{
+            checkID = false;
+            errMesID = "กรุณาใส่รหัสบัตรประชาชนให้ครบ 13 หลัก";
+        }
+
+        if(id.length <= 10){
+            checkID = true;
+        }else{
+            checkID = false;
+            errMesID = "กรุณาใส่รหัสบัตรประชาชนไม่เกิน 13 หลัก";
+        }
+
+        if(tel.length >= 10){
+            checkTel = true;
+        }else{
+            checkTel = false;
+            errMesTel = "กรุณาใส่เบอร์โทรให้ครบ 10 หลัก";
+        }
+
+        if(tel.length <= 10){
+            checkTel = true;
+        }else{
+            checkTel = false;
+            errMesTel = "กรุณาใส่เบอร์โทรไม่เกิน 10 หลัก";
+        }
 
         if(username.length >= 8){
             checkUsername = true;
@@ -34,11 +70,13 @@ document.addEventListener("DOMContentLoaded", function(){
             errMesConfirmPassword = "กรุณาใส่รหัสผ่านให้ตรงกัน";
         } 
 
+        document.querySelector("#alertID").innerHTML = errMesID;
+        document.querySelector("#alertTel").innerHTML = errMesTel;
         document.querySelector("#alertUsername").innerHTML = errMesUsername;
         document.querySelector("#alertPassword").innerHTML = errMesPassword;
         document.querySelector("#alertConfirmPassword").innerHTML = errMesConfirmPassword;
         
-        if((checkUsername & checkPassword) == true){
+        if((checkID & checkTel & checkUsername & checkPassword & checkConfirmPassword) == true){
             check = true;
         }
         //check = checkUsername & checkPassword;
